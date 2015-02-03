3a83ae50b1b4c0776d1c989bf59b09df32f406e9
mjones <matthew7j@aol.com>
2015-02-03 13:24:17 -0500

    Getting Set-up, Testing Commits.

---
+++ src/com/Blackjack_Game/Blackjack.java
@@ -0,0 +1,9 @@
+package com.Blackjack_Game;
+
+public class Blackjack {
+
+    public static void main(String[] args)
+    {
+
+    }
+}
--- src/com/matthew7j/Main.java
+++
@@ -1,8 +0,0 @@
-package com.matthew7j;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
