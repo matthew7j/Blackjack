75814d33db73bcffd34d26c3e169f9475babe5e7
mjones <matthew7j@aol.com>
2015-02-03 15:33:51 -0500

    Created Value and Suit Enums for Card Values Adding GUI for user inputs Adding button functionality

--- src/com/matthew7j/Blackjack_GUI.java
+++ src/com/matthew7j/Blackjack_GUI.java
@@ -1,11 +1,13 @@
 package com.matthew7j;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Blackjack_GUI extends JFrame {
     private JPanel blackjackPanel;
-    private JSlider slider1;
-    private JSlider slider2;
+    private JSlider playerSlider;
+    private JSlider deckSlider;
     private JButton startButton;
 
     public Blackjack_GUI() {
@@ -15,6 +17,16 @@ public class Blackjack_GUI extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setSize(400, 200);
+
+        startButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                int numPlayers = playerSlider.getValue();
+                int numDecks = deckSlider.getValue();
+
+                Game game = new Game(numPlayers, numDecks);
+            }
+        });
     }
 
 }
---
+++ src/com/matthew7j/Game.java
@@ -0,0 +1,10 @@
+package com.matthew7j;
+
+public class Game {
+    int decks, players;
+
+    public Game(int players, int decks) {
+        this.players = players;
+        this.decks = decks;
+    }
+}
