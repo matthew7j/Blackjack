8ef1729b4826455c63c29aebde27e5a2e651ab3a
mjones <matthew7j@aol.com>
2015-02-04 15:45:38 -0500

    Took out GUI. Going to use another framework to make it look better.

--- src/com/matthew7j/Game.java
+++ src/com/matthew7j/Game.java
@@ -1,14 +1,15 @@
 package com.matthew7j;
 
+import java.util.ArrayList;
+
 public class Game {
     int decks;
+    ArrayList<Player> players = new ArrayList<Player>();
 
     public Game(int decks) {
         this.decks = decks;
 
         initGame();
-
-        new Table();
     }
 
     private void initGame() {
@@ -20,5 +21,9 @@ public class Game {
         shoe.shuffle();
 
         System.out.println(shoe.toString());
+    }
+
+    public void addPlayer(double money) {
+        this.players.add(new Player(money));
     }
 }
--- src/com/matthew7j/Main.java
+++ src/com/matthew7j/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Blackjack_GUI gui = new Blackjack_GUI();
-
+        Game game = new Game(5);
     }
 }
